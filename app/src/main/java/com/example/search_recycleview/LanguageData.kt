90dda592b0af7c935c0daf374e341b10f0c45bff
renggadiansa <renggadiansa@gmail.com>
2022-12-05 07:09:11 +0700

    add asset

---
+++ app/src/main/java/com/example/search_recycleview/LanguageData.kt
@@ -0,0 +1,3 @@
+package com.example.search_recycleview
+
+data class LanguageData(val title : String, val logo : Int)
--- app/src/main/java/com/example/search_recycleview/MainActivity.kt
+++ app/src/main/java/com/example/search_recycleview/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.search_recycleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView : RecyclerView
     private lateinit var searchView: SearchView
+    private var mList= ArrayList<LanguageData>()
+    private lateinit var adapter: LanguageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +23,47 @@ class MainActivity : AppCompatActivity() {
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(this)
         addDataToList()
+
+        adapter = LanguageAdapter(mList)
+        recyclerView.adapter = adapter
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterList(newText)
+                return true
+            }
+
+        })
+    }
+
+    private fun filterList(query : String?){
+        if(query != null){
+            val filteredList = ArrayList<LanguageData>()
+            for(item in mList){
+                if (item.title.lowercase(Locale.ROOT).contains(query)) {
+                    filteredList.add(item)
+                }
+            }
+           if (filteredList.isEmpty()){
+               Toast.makeText(this, "Data tidak tersedia", Toast.LENGTH_SHORT).show()
+           }else{
+                adapter.setFilteredList(filteredList)
+           }
+        }
     }
 
     private fun addDataToList() {
-        TODO("Not yet implemented")
+        mList.add(LanguageData("JAVA" , R.drawable.java))
+        mList.add(LanguageData("KOTLIN" , R.drawable.kotlin))
+        mList.add(LanguageData("C++" , R.drawable.cplusplus))
+        mList.add(LanguageData("HTML" , R.drawable.html))
+        mList.add(LanguageData("PYTHON" , R.drawable.python))
+        mList.add(LanguageData("SWIFT" , R.drawable.swift))
+        mList.add(LanguageData("C#" , R.drawable.csharp))
+        mList.add(LanguageData("JAVASCRIPT" , R.drawable.javascript))
     }
 }
